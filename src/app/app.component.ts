340f4b3d6c3361bb2e40f6738819893b02ac019e
Javier Alejandro Castañeda Celis <desarrollo@bitu.com.co>
2024-11-13 22:00:37 -0500

    Login example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatLabel
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
