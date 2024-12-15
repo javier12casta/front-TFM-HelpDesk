f5738bfe13d7f531459b1e63c6791b08fc553298
Javier Alejandro Castañeda Celis <desarrollo@bitu.com.co>
2024-12-14 19:36:54 -0500

    Ajuste rutas APP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { HomeComponent } from './modules/home/home.component';
 
 export const routes: Routes = [
   {
@@ -9,6 +11,12 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
       { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
+  },
+  { path: 'app',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
     ]
   },
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
